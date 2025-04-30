785d0f4c3d82d35cc47b56fe43fb519ef5ec20e8
Makenzo04 <madiyar.talgatuly@inbox.ru>
2025-04-30 17:17:11 +0500

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from 'next';
-import { Geist_Mono } from 'next/font/google';
 import './globals.css';
 import './fonts.css';
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
 
 export const metadata: Metadata = {
   title: 'Bframe',
