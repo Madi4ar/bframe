047406f14bfe1472d5e9f4cd631c5baa5ab6dbe6
Makenzo04 <madiyar.talgatuly@inbox.ru>
2025-05-05 10:55:20 +0500

    fix: some bugs with sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,12 +14,12 @@ export default function Home() {
   return (
     <div className="bg-[#1C1D21] min-h-screen flex flex-col md:flex-row">
       <div
-        className={`border z-50 w-full h-screen md:w-[20%] fixed transition-all duration-300 ${
+        className={`border z-50 w-full h-screen md:w-[15%] fixed transition-all duration-300 ${
           isSidebarOpen ? 'left-0 h-screen' : '-left-full'
         } md:left-0 md:relative border-r border-[#28292D]`}>
         <Sidebar onClose={toggleSidebar} />
       </div>
-      <div className="w-full md:w-[80%] p-[16px]">
+      <div className="w-full md:w-[85%] p-[16px]">
         <MainPage onMenuClick={toggleSidebar} />
       </div>
     </div>
